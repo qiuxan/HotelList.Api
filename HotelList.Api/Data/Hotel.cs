d44644bf974f60edb4424f85758859643ca3a753
qiuxan <qiuxan@gmail.com>
2025-12-07 22:28:57 +1100

    Init the data

---
+++ HotelList.Api/Data/Hotel.cs
@@ -0,0 +1,10 @@
+namespace HotelList.Api.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+}
